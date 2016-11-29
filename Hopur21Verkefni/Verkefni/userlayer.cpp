01e7454ee7cc5624830c64cdf809269d56adb8e8
gudrunvald <gudrunv16@ru.is>
2016-11-29 15:54:06 +0000

    endl

--- Hopur21Verkefni/Verkefni/userlayer.cpp
+++ Hopur21Verkefni/Verkefni/userlayer.cpp
@@ -15,17 +15,15 @@ void UserLayer::addPerson()
     cout << "Enter name: ";
     cin >> name;
     // TODO :IF PERSON EXISTS DISPLAY ERROR MESSAGE
-    cout << endl;
     cout << "Enter gender: ";
     cin >> gender;
-    cout << endl;
     cout << "Enter the year of birth: ";
     cin >> birthYear;
-    cout << endl;
     cout << "Enter the year of death: ";
     cin >> deathYear;
     cout << "Enter information about the person: ";
     cin >> comment;
+    cout << endl;
 
     //CSPerson newPerson(name, gender, birthYear, deathYear, comment); TODO Lesa inn nýjan Person
 
