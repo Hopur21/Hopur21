f8599bf1fb1b79838d8e250dffddcd7ca4eaead0
David Haukur Þorgilsson <Davidhaukur@Davids-MacBook-Pro.local>
2016-11-29 16:04:17 +0000

    minor changes made to CSPersonService

--- Hopur21Verkefni/Verkefni/cspersonservice.cpp
+++ Hopur21Verkefni/Verkefni/cspersonservice.cpp
@@ -5,9 +5,9 @@ CSPersonService::CSPersonService()
 
 }
 
-void newPerson(string name, string gender, int birthYear, int deathYear, string comment)
+void CSPersonService::newPerson(string name, string gender, int birthYear, int deathYear, string comment)
 {
-    CSPerson newPerson(name, gender, birthYear, deathYear, comment);
+    CSPerson newP = CSPerson (name, gender, birthYear, deathYear, comment);
     /*newPerson._name = name;
     newPerson._gender = gender;
     newPerson._birthYear = birthYear;
