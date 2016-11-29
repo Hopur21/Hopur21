c94c9e4386aa50776bb111da876c39974c2d474a
gudrunvald <gudrunv16@ru.is>
2016-11-29 11:14:38 +0000

    submenu for display menu

--- Hopur21Verkefni/Verkefni/menuinterface.cpp
+++ Hopur21Verkefni/Verkefni/menuinterface.cpp
@@ -36,12 +36,46 @@ void MenuInterface::processChoice(int choice)
     // Here the input from the main menu is processed
     if(coice == 1)
     {
+        // SETJA ALLT ÞETTA DRASL Í SÉR FÖLL!!!!
         //sub menu for different display options
         //in alphabetical order
         //by age
         //by year of birth
         //by year of death
+        int choice = 0;
+
+        cout << "Enter 1 to display the complete list in alphabetical order" << endl;
+        cout << "Enter 2 to display the complete list by age of Computer Scientist" << endl;
+        cout << "Enter 3 to display the complete list by year of birth" << endl;
+        cout << "Enter 4 to display the complete list by year of death" << endl;
+        cin >> choice;
+
+        if(choice == 1)
+        {
+            cout << "Computer Scientists in alphabetical order:" << endl;
+            cout << "******************************************" << endl;
+            //funtion sort alphabetical
+        }
+        else if(choice == 2)
+        {
+            cout << "Computer Scientists by age:" << endl;
+            cout << "***************************" << endl;
+            //funtion sort by age, age = deathYear-birthYear
+        }
+        else if(choice == 3)
+        {
+            cout << "Computer Scientists by year of birth:" << endl;
+            cout << "*************************************" << endl;
+            //funtion sort by year of birth
+        }
+        else if(choice == 4)
+        {
+            cout << "Computer Scientists by year of death:" << endl;
+            cout << "*************************************" << endl;
+            //funtion sort by year of death
+        }
     }
+
     else if(choice == 2)
     {
         person.addPerson();
