efd38d7eb322c11e71e08e1d1fb6c8f16a29da0a
gudrunvald <gudrunv16@ru.is>
2016-11-29 11:26:23 +0000

    ble

--- Hopur21Verkefni/Verkefni/menuinterface.cpp
+++ Hopur21Verkefni/Verkefni/menuinterface.cpp
@@ -13,10 +13,10 @@ void MenuInterface::DisplayMenu()
 
     // THIS MENU IS NOT COMPLETE YET!!!!!
 
-    cout << "Enter 1 to display the complete list in alphabetical order" << endl;
-    cout << "Enter 2 to add a computer scientist to the list" << endl;
-    cout << "Enter 3 to search the list for a computer scientist" << endl;
-    cout << "Enter 4 to exit program" << endl;
+    cout << "Enter 1 to display the complete list in alphabetical order " << endl;
+    cout << "Enter 2 to add a computer scientist to the list " << endl;
+    cout << "Enter 3 to search the list for a computer scientist " << endl;
+    cout << "Enter 4 to exit program " << endl;
     cin >> choice;
 
     if(choice == 1 || choice == 2 || choice == 3 || choice ==4)
@@ -34,7 +34,7 @@ void MenuInterface::DisplayMenu()
 void MenuInterface::processChoice(int choice)
 {
     // Here the input from the main menu is processed
-    if(coice == 1)
+    if(choice == 1)
     {
         // SETJA ALLT ÞETTA DRASL Í SÉR FÖLL!!!!
         //sub menu for different display options
@@ -42,33 +42,33 @@ void MenuInterface::processChoice(int choice)
         //by age
         //by year of birth
         //by year of death
-        int choice = 0;
 
-        cout << "Enter 1 to display the complete list in alphabetical order" << endl;
-        cout << "Enter 2 to display the complete list by age of Computer Scientist" << endl;
-        cout << "Enter 3 to display the complete list by year of birth" << endl;
-        cout << "Enter 4 to display the complete list by year of death" << endl;
-        cin >> choice;
+        cout << "Enter 1 to display the complete list in alphabetical order " << endl;
+        cout << "Enter 2 to display the complete list by age of Computer Scientist " << endl;
+        cout << "Enter 3 to display the complete list by year of birth " << endl;
+        cout << "Enter 4 to display the complete list by year of death " << endl;
+        int choiceSub = 0;
+        cin >> choiceSub;
 
-        if(choice == 1)
+        if(choiceSub == 1)
         {
             cout << "Computer Scientists in alphabetical order:" << endl;
             cout << "******************************************" << endl;
             //funtion sort alphabetical
         }
-        else if(choice == 2)
+        else if(choiceSub == 2)
         {
             cout << "Computer Scientists by age:" << endl;
             cout << "***************************" << endl;
             //funtion sort by age, age = deathYear-birthYear
         }
-        else if(choice == 3)
+        else if(choiceSub == 3)
         {
             cout << "Computer Scientists by year of birth:" << endl;
             cout << "*************************************" << endl;
             //funtion sort by year of birth
         }
-        else if(choice == 4)
+        else if(choiceSub == 4)
         {
             cout << "Computer Scientists by year of death:" << endl;
             cout << "*************************************" << endl;
