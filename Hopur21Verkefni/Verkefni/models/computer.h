f5d5ebb4928d747faad2a225aab797e2cb591e22
David Haukur Þorgilsson <Davidhaukur@Davids-MacBook-Pro.local>
2016-12-05 14:42:52 +0000

    gets & sets for computer class created

--- Hopur21Verkefni/Verkefni/models/computer.h
+++ Hopur21Verkefni/Verkefni/models/computer.h
@@ -15,6 +15,20 @@ class Computer
 
     public:
         Computer();
+
+        // Gets
+        string getName()const {return _name;}
+        int getDesignYear()const {return _designYear;}
+        int getBuildYear()const {return _buildYear;}
+        int getTypeID()const {return _typeID;}
+        bool getCreated()const {return _created;}
+
+        //Sets
+        void setName(const string name){_name = name;}
+        void setDesignYear(const int designYear){_designYear = designYear;}
+        void setBuildYear(const int buildyear){_buildYear = buildyear;}
+        void setTypeID(const int type){_typeID = type;}
+        void setCreated(const bool created){_created = created;}
 };
 
 #endif // COMPUTER_H
