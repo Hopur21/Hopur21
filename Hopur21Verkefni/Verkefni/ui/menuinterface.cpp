2ea25f572fe8479cafa2ac65b53748ef8019f83e
Nonni <nonni@nocando.is>
2016-12-09 19:46:33 +0000

    minor fix for menuinterface

--- Hopur21Verkefni/Verkefni/ui/menuinterface.cpp
+++ Hopur21Verkefni/Verkefni/ui/menuinterface.cpp
@@ -145,16 +145,16 @@ void MenuInterface::processChoice(const string choice)
         cout << "| 2 | Scientist " << endl;
         cin >> subMenuChoice;
 
+        if(subMenuChoice == "1")
+            cout << "hommz" << endl;
+
         if(subMenuChoice != "1" || subMenuChoice != "2")
         {
             invalidInput();
         }
-        else
-        {
-            cout << "HALLO FROM OKOKOK";
-        }
-    }
 
+    }
+    cout << "out of the loop"<< endl;
     int subMenuInt = stoi(subMenuChoice);
     int choiceInt = stoi(choice);
     banner();
