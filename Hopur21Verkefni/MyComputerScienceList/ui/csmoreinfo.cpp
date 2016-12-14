0ee3eb76a829ac2c846f6140a47bb6ba0cbce9ce
Nonni <nonni@nocando.is>
2016-12-14 18:26:50 +0000

    Adding and reading picture from mysql

--- Hopur21Verkefni/MyComputerScienceList/ui/csmoreinfo.cpp
+++ Hopur21Verkefni/MyComputerScienceList/ui/csmoreinfo.cpp
@@ -15,4 +15,30 @@ CSMoreInfo::~CSMoreInfo()
 void CSMoreInfo::setComputerScientist(CSPerson myPerson)
 {
     ui->label_tempNafn->setText(QString::fromStdString(myPerson.getName()));
+
+    // Alternatively, load an image file directly into a QByteArray
+    QFile file("test.jpg");
+    if (file.open(QIODevice::ReadOnly))
+    {
+        qDebug() << "nadi að opna file";
+    }
+    QByteArray inByteArray = myPerson.getImage();
+
+       QPixmap outPixmap = QPixmap();
+       outPixmap.loadFromData( inByteArray );
+
+       ui->showPicLabel->setPixmap(outPixmap);
+       ui->showPicLabel->setScaledContents(true);
+       ui->showPicLabel->show();
+
+
+       /*
+       ui->label_title->setText(QString("<h2>%1</h2>").arg(QString::fromStdString(car.toString())));
+       QPixmap pixmap(QString::fromStdString(car.getImagePath()));
+       ui->label_image->setPixmap(pixmap);
+       ui->label_image->setScaledContents(true);
+       */
+
+
+
 }
