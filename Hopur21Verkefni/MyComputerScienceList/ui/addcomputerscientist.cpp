2be1f9a54fd7bea53e0f4e7f75154d2e921f93b5
David Haukur Þorgilsson <Davidhaukur@Davids-MacBook-Pro.local>
2016-12-14 09:48:05 +0000

    addComputerScientist now has some validation through QIntValidator

--- Hopur21Verkefni/MyComputerScienceList/ui/addcomputerscientist.cpp
+++ Hopur21Verkefni/MyComputerScienceList/ui/addcomputerscientist.cpp
@@ -6,6 +6,17 @@ AddComputerScientist::AddComputerScientist(QWidget *parent) :
     ui(new Ui::AddComputerScientist)
 {
     ui->setupUi(this);
+
+    // getCurrent year   (year is the currentyear)
+    time_t timeNow = time(0);
+    tm *ltm = localtime(&timeNow);
+    int year = ltm->tm_year + 1900;
+
+    // Validation for year of birth & year of death
+    QIntValidator *yearValidator = new QIntValidator(0,year);
+    ui->lineEdit_Addscientist_yearofbirth->setValidator(yearValidator);
+    ui->lineEdit_Addscientist_deathYear->setValidator(yearValidator);
+
 }
 
 AddComputerScientist::~AddComputerScientist()
@@ -15,47 +26,67 @@ AddComputerScientist::~AddComputerScientist()
 
 void AddComputerScientist::on_pushButton_Addscientist_save_clicked()
 {
+    // While loop conditions
+    bool exitLoop;
+    bool validInput;
+
+    // Gender options
+    bool male = false;
+    bool female = false;
+    bool other = false;
+
     QString name = "";
+    // isAlive is connected to the checkbox
     bool isAlive;
     QString gender;
     QString comment;
     QString birthYear;
     QString deathYear;
 
-    // getCurrent year   (year is the currentyear)
-    time_t timeNow = time(0);
-    tm *ltm = localtime(&timeNow);
-    int year = ltm->tm_year + 1900;
-
 
     // Taka inn mynd
+
+
+    bool canCreatePerson = true;
+
 
     name = ui->lineEdit_Addscientist_name->text();
     qDebug() << name;
     birthYear = ui->lineEdit_Addscientist_yearofbirth->text();
+    qDebug() << birthYear;
     isAlive = ui->checkBox_Addscientist_isPersonAlive->isChecked();
-
-    if(isAlive == false)
+    if(isAlive)
+    {
+        qDebug() << "This person is still alive";
+    }
+    else if(isAlive == false)
     {
         deathYear = ui->lineEdit_Addscientist_deathYear->selectedText();
+        qDebug() << "This person died in the year" << deathYear;
     }
 
-    bool saveStatus = true;
-
-    // If name is an empty string
-    if(name == "")
+    // If name or birthYear are empty strings
+    if(name == "" || birthYear == "")
     {
-        saveStatus = false;
+        canCreatePerson = false;
     }
-    // QIntValidator á eftir að skoða betur
-    //ui->lineEdit_Addscientist_yearofbirth->setValidator( new QIntValidator(0, year, this) );
 
-
-    CSPerson createdPerson;
-    _newPerson = createdPerson;
+    if(isAlive == false && deathYear == "")
+    {
+        canCreatePerson = false;
+    }
 
     // 1 denotes success 0 denotes failure
-    setResult(QDialog::Accepted);
+    if(canCreatePerson == true)
+    {
+        CSPerson createdPerson;
+        _newPerson = createdPerson;
+        //setResult(QDialog::Accepted);
+    }
+    else
+    {
+        //setResult(QDialog::Rejected);
+    }
 
 
 }
